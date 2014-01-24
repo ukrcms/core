04af7996175568877b5b76d148d6ce935ad62d8a
funivan <dev@funivan.com>
2014-01-24 15:30:11 +0200

    Different db connection in Travis-Ci and localhost for test database

--- dev/UcDemo/CommonApp/config.php
+++ dev/UcDemo/CommonApp/config.php
@@ -1,10 +1,11 @@
 <?php
+  $travis = !empty($_ENV['TRAVIS']);
   return array(
     'config' => array(),
     'db' => array(
       'dsn' => 'mysql:host=localhost;dbname=ukrcms_core_test',
-      'username' => 'root',
-      'password' => '1111',
+      'username' => $travis ? 'travis' : 'root',
+      'password' => $travis ? '' : '1111',
       'tablePrefix' => 'uc_',
     ),
   );
